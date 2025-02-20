4cc7db480686411fef5bd666277be79b6f59ed32
William Alexander <williamalexander@Williams-MacBook-Air.local>
2025-02-21 00:50:58 +0700

    run sonarcloud from gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,17 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.3.8"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonarqube {
+	properties {
+		property("sonar.projectKey", "williamalxndr_adpro-eshop")
+		property("sonar.organization", "williamalxndr")
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.token", System.getenv("SONARCLOUD_TOKEN"))
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+	}
 }
 
 group = "id.ac.ui.cs.advprog"
