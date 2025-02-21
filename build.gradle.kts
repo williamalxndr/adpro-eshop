553e8d690e0b7e4384fe4c5e51566d3d91bfc685
William Alexander <williamalexander@Williams-Air.bbrouter>
2025-02-21 16:33:50 +0700

    run sonar (version 6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.3.8"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.sonarqube") version "5.0.0.4638"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 sonar {
