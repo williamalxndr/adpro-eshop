604d47374ed6a1cbe1548bff9520c59ade227165
William Alexander <williamalexander@Williams-MacBook-Air.local>
2025-03-06 19:10:28 +0700

    [GREEN] Implement Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -81,8 +81,8 @@ class OrderTest {
 
     @Test
     void testSetStatusToInvalidStatus() {
-        Order order = new Order( id: "13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, orderTime: 1708560000L, author: "Safira Sudrajat");
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products,1708560000L,"Safira Sudrajat");
         assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
     }
 
