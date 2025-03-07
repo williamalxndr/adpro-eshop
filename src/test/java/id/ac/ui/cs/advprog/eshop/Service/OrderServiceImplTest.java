43be46552a9f2092ad56ee48f9c2585c53cff2bf
William Alexander <williamalexander@Williams-MacBook-Air.local>
2025-03-07 15:40:10 +0700

    [RED] Add OrderServiceImpl skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/Service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/Service/OrderServiceImplTest.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+import id.ac.ui.cs.advprog.eshop.service.OrderServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
